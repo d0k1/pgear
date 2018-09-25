e317ef2d05f3ecbae38351901f23e3b4b550058b
Denis Kirpichenkov <dkirpichenkov@naumen.ru>
2018-09-25 18:56:21 +0500

    a better histogram builder

--- src/Histogram.groovy
+++ src/Histogram.groovy
@@ -9,6 +9,10 @@
 )
 
 import org.apache.commons.math3.random.EmpiricalDistribution
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics
+
+DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics();
+EmpiricalDistribution distribution = new EmpiricalDistribution();
 
 String csvFilename = args[0];
 
@@ -21,14 +25,17 @@ def times = [];
 
 while ( (line = reader.readLine()) != null) {
     def items = line.split("\\;");
-    times << items[4];
+    double time = Double.parseDouble(items[5])
+
+    descriptiveStatistics.addValue(time);
+    times << time;
 }
 
 File output = new File("histo.csv");
 output.delete();
 output = new File("histo.csv");
 
-EmpiricalDistribution distribution = new EmpiricalDistribution();
+
 distribution.load(times as double[])
 def stat = distribution.getBinStats();
 output << "Bin;Freq"+"\n";
@@ -38,8 +45,30 @@ stat.each{ it->
         output << "$it.min..$it.max;$it.n"+"\n";
     }
 }
+output<<"\nmin;50%;95%;99%;99.9%;max;stddev;n\n"
+
+output << descriptiveStatistics.min <<";"
+output << descriptiveStatistics.getPercentile(50.0) <<";"
+output << descriptiveStatistics.getPercentile(95.0) <<";"
+output << descriptiveStatistics.getPercentile(99.0) <<";"
+output << descriptiveStatistics.getPercentile(99.9) <<";"
+output << descriptiveStatistics.max <<";"
+output << descriptiveStatistics.standardDeviation <<";"
+output << descriptiveStatistics.n <<";"
 
 println "Queries total: "+times.size();
 println "Sum: "+distribution.sampleStats.sum.toLong();
+print "\nmin;50%;95%;99%;99.9%;max;stddev;n\n";
+print  descriptiveStatistics.min+";"
+print  descriptiveStatistics.getPercentile(50.0) +";"
+print  descriptiveStatistics.getPercentile(95.0) +";"
+print  descriptiveStatistics.getPercentile(99.0) +";"
+print  descriptiveStatistics.getPercentile(99.9) +";"
+print  descriptiveStatistics.max+";"
+print  descriptiveStatistics.standardDeviation +";"
+print  descriptiveStatistics.n+";"
+println ""
+
+
 
 
